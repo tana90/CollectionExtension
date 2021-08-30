9cc1612c02bb67560e807bc5280842bd1e6bc58e
tana90 <iamtana90@gmail.com>
2021-08-30 12:33:59 +0300

    Added .sliced extension to Array

--- Sources/CollectionExtension/CollectionExtension.swift
+++ Sources/CollectionExtension/CollectionExtension.swift
@@ -37,6 +37,20 @@ public extension Array where Element: Equatable {
     }
     
     @discardableResult
+    func sliced(by dateComponents: Set<Calendar.Component>,
+                for key: KeyPath<Element, Date>) -> [Date: [Element]] {
+        let initial: [Date: [Element]] = [:]
+        let groupedByDateComponents = reduce(into: initial) { acc, cur in
+            let components = Calendar.current.dateComponents(dateComponents, from: cur[keyPath: key])
+            let date = Calendar.current.date(from: components)!
+            let existing = acc[date] ?? []
+            acc[date] = existing + [cur]
+        }
+        
+        return groupedByDateComponents
+    }
+    
+    @discardableResult
     mutating func removeAll(_ item: Element) -> [Element] {
         removeAll(where: { $0 == item })
         return self
